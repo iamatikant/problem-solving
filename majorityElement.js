8d00de54bf7a6da9d3dbef3624bf521bd65a93f9
Atikant <atikant.ati@gmail.com>
2024-03-31 14:27:59 +0530

    Fixes vscode settings.

--- majorityElement.js
+++ majorityElement.js
@@ -31,40 +31,39 @@ Follow-up: Could you solve the problem in linear time and in O(1) space?
  * @param {number[]} nums
  * @return {number}
  */
-var majorityElement = function(nums) {
+var majorityElement = function (nums) {
   //My code
-    // const targetOccurrence = nums.length / 2;
-    // if(nums.length <= 1) {
-    //   return nums[0];
-    // }
-    // const dummy = {};
-    // for(let num of nums) {
-    //   if(dummy[num] === undefined) {
-    //     dummy[num] = 1;
-    //   } else {
-    //     dummy[num] = dummy[num] + 1;
-    //     if(dummy[num] > targetOccurrence) {
-    //       return num;
-    //     }
-    //   }
-    // }
-    // Sample
-    debugger;
-    let candidate = nums[0]
-    let counter = 1
-    for(let i = 1 ; i < nums.length ; i++) {
-        if(nums[i] === candidate) {
-            counter += 1
-        } else {
-            counter -= 1
-            if(counter === 0) {
-                candidate = nums[i]
-                counter = 1
-            }
-        }
+  // const targetOccurrence = nums.length / 2;
+  // if(nums.length <= 1) {
+  //   return nums[0];
+  // }
+  // const dummy = {};
+  // for(let num of nums) {
+  //   if(dummy[num] === undefined) {
+  //     dummy[num] = 1;
+  //   } else {
+  //     dummy[num] = dummy[num] + 1;
+  //     if(dummy[num] > targetOccurrence) {
+  //       return num;
+  //     }
+  //   }
+  // }
+  // Sample
+  debugger;
+  let candidate = nums[0];
+  let counter = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === candidate) {
+      counter += 1;
+    } else {
+      counter -= 1;
+      if (counter === 0) {
+        candidate = nums[i];
+        counter = 1;
+      }
     }
-    return candidate
+  }
+  return candidate;
 };
 
-
-console.log(majorityElement([2,2,1,1,1,2,2]));
+console.log(majorityElement([2, 2, 1, 1, 1, 2, 2]));
