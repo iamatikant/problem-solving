7f5718484b557741c1813e9028db951fe26d2579
Atikant <atikant.ati@gmail.com>
2024-07-17 21:59:51 +0530

    Currying function level 1.

---
+++ js/problems/curry/curry.js
@@ -0,0 +1,15 @@
+const curry = () => {
+  let result = 0;
+  return (num) => {
+    return num ? (result += num) : result;
+  };
+};
+
+//Returns and store the inner function.
+let sum = curry();
+
+console.log(sum(5)); //5
+console.log(sum(3)); //8
+console.log(sum(4)); //12
+console.log(sum(0)); //12
+console.log(sum()); //12
