cb15c9b119542876ceb583fea0342203e9d89617
Atikant <atikant.ati@gmail.com>
2024-07-24 23:00:04 +0530

    Currying fix

--- js/problems/curry_2/curry_2.js
+++ js/problems/curry_2/curry_2.js
@@ -11,20 +11,20 @@ console.log(res, res2, res3, res4, res5, res6);
 // Output:
 // 10 10 10 10 10 0
 
-function sum(...args) {
-  let result = [...args];
-  if (!result.length) {
-    return result.reduce((prev, curr) => prev + curr, 0);
-  }
-  const helper = (...args2) => {
-    result = [...result, ...args2];
-    if (!args2.length) {
-      return result.reduce((prev, curr) => prev + curr, 0);
-    }
-    return helper;
-  };
-  return helper;
-}
+// function sum(...args) {
+//   let result = [...args];
+//   if (!result.length) {
+//     return result.reduce((prev, curr) => prev + curr, 0);
+//   }
+//   const helper = (...args2) => {
+//     result = [...result, ...args2];
+//     if (!args2.length) {
+//       return result.reduce((prev, curr) => prev + curr, 0);
+//     }
+//     return helper;
+//   };
+//   return helper;
+// }
 
 /**
  *
@@ -52,18 +52,18 @@ function sum(...args) {
  *
  */
 
-// function sum(...args) {
-//   let result = [...args];
-//   if (!args.length) {
-//     return result.reduce((prev, curr) => prev + curr, 0);
-//   }
-//   const helper = (...args2) => {
-//     result = [...result, ...args2];
-//     if (args2.length === 0) {
-//       return result.reduce((prev, cur) => prev + cur, 0);
-//     } else {
-//       return helper;
-//     }
-//   };
-//   return helper;
-// }
+function sum(...args) {
+  let result = [...args];
+  if (!args.length) {
+    return result.reduce((prev, curr) => prev + curr, 0);
+  }
+  const helper = (...args2) => {
+    result = [...result, ...args2];
+    if (args2.length === 0) {
+      return result.reduce((prev, cur) => prev + cur, 0);
+    } else {
+      return helper;
+    }
+  };
+  return helper;
+}
