55935299f46432da7d7a334355218a6ae07410c4
Atikant <atikant.ati@gmail.com>
2024-07-26 23:54:52 +0530

    Curry 3

--- js/problems/curr_3/curr3.js
+++ js/problems/curr_3/curr3.js
@@ -8,20 +8,40 @@ console.log(curriedSum(1, 2, 3, 4, 5)); // 10
 console.log(curriedSum(1)(2, 3)(4, 5)); // 10
 console.log(curriedSum(1)(2)(3)(4)); // 10
 
-function curry() {
-  return (...args1) => {
-    let result = [...args1];
-    let total = 0;
-    if (!result.length) {
-      total = result.reduce((prev, next) => prev + next, 0);
+// function curry(fn) {
+//   return (...args1) => {
+//     let result = [...args1];
+//     let total = 0;
+//     if (!result.length) {
+//       return total;
+//     } else {
+//       total = result.reduce((prev, next) => prev + next, 0);
+//     }
+//     const helper = (...args2) => {
+//       result = [...result, ...args2];
+//       if (args2.length === 0 || !result.length) {
+//         return total;
+//       }
+//       total = result.reduce((prev, next) => prev + next, 0);
+//     };
+//     if (result.length < 4) {
+//       return helper;
+//     } else return total;
+//   };
+// }
+
+function curry(fn) {
+  let helper = (...arg1) => {
+    let result = [...arg1];
+    if (result.length >= fn.length) {
+      return fn(...result);
     }
-    const helper = (...args2) => {
+
+    let repeat = (...args2) => {
       result = [...result, ...args2];
-      if (!result.length) {
-        return total;
-      }
-      total = result.reduce((prev, next) => prev + next, 0);
+      return result;
     };
-    return helper;
+    return repeat;
   };
+  return helper;
 }
