14b7bb9d22000b289c3f01ef1284d20e8c553e8b
Atikant <atikant.ati@gmail.com>
2024-07-29 17:38:55 +0530

    Better code.

--- problems-solving_DSAlgo/filter_element/filter.js
+++ problems-solving_DSAlgo/filter_element/filter.js
@@ -1,22 +1,21 @@
 const test = [1, 2, 2, 3, 3, 4, 5, 6];
+// const test = [1, 1, 2];
 
 // op: [1,2,3,4,5,6,0,0], 6
 
-function filter_elements(arr) {
-  let length = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    const curr = arr[i];
-    const next = arr[i + 1];
-    if (next !== curr) {
-      length++;
-    } else {
-      arr.splice(i + 1, 1);
-      arr.push(0);
+var removeDuplicates = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    const curr = nums[i];
+    const next = nums[i + 1];
+    if (next === curr) {
+      nums.splice(i + 1, 1);
+      i--;
     }
   }
+  return nums;
+};
+// function filter(arr) {
 
-  return arr;
-}
+// }
 
-console.log(filter_elements(test));
+console.log(removeDuplicates(test));
