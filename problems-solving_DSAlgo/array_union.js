d2d8c10ccdd76d2a2ca2770f949ed0334640bc59
Atikant <atikant.ati@gmail.com>
2024-08-03 23:21:47 +0530

    Find union of arrays.

---
+++ problems-solving_DSAlgo/array_union.js
@@ -0,0 +1,24 @@
+const ip1 = [1, 2, 2, 2, 3, 3, 5, 6, 7, 1];
+const ip2 = [1, 2, 2, 10, 1, 1];
+// op: [1, 2, 2, 1]
+
+function findUnion(arr1, arr2) {
+  const map = new Map();
+  const result = [];
+  for (const element of arr1) {
+    if (map.has(element)) {
+      map.set(element, map.get(element) + 1);
+    } else {
+      map.set(element, 1);
+    }
+  }
+  for (const element of arr2) {
+    if (map.has(element) && map.get(element) >= 1) {
+      result.push(element);
+      map.set(element, map.get(element) - 1);
+    }
+  }
+  return result;
+}
+
+console.log(findUnion(ip1, ip2));
