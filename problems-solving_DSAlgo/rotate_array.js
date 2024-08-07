3c727d3f018a12a6a28dec16c003f11738021ccb
Atikant <atikant.ati@gmail.com>
2024-08-07 23:45:59 +0530

    Mock commit

--- problems-solving_DSAlgo/rotate_array.js
+++ problems-solving_DSAlgo/rotate_array.js
@@ -1,5 +1,4 @@
 // Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
-// this is going as a dummy commit.
 
 // Input: nums = [1,2,3,4,5,6,7], k = 3
 // Output: [5,6,7,1,2,3,4]
