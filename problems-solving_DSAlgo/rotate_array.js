0e64b87064c68884aa21e8bb96d0c39d164cfd8f
Atikant <atikant.ati@gmail.com>
2024-08-08 23:45:23 +0530

    This is another commit.

--- problems-solving_DSAlgo/rotate_array.js
+++ problems-solving_DSAlgo/rotate_array.js
@@ -1,4 +1,5 @@
 // Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+//this is nothing. I could not get much time today.
 
 // Input: nums = [1,2,3,4,5,6,7], k = 3
 // Output: [5,6,7,1,2,3,4]
