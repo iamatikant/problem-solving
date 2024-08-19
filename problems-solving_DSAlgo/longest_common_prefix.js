8ed6092e7655823f45b67bec7558528ff2e3dbac
Atikant <atikant.ati@gmail.com>
2024-08-19 22:52:36 +0530

    Dummy

--- problems-solving_DSAlgo/longest_common_prefix.js
+++ problems-solving_DSAlgo/longest_common_prefix.js
@@ -13,7 +13,6 @@
 // Explanation: There is no common prefix among the input strings.
 
 // Constraints:
-
 // 1 <= strs.length <= 200
 // 0 <= strs[i].length <= 200
 // strs[i] consists of only lowercase English letters.
