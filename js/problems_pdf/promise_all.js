b54cb7ce9dc2f1bccf20621193dbda283224eecd
Atikant <atikant.ati@gmail.com>
2024-08-20 23:49:48 +0530

    Promise all polyfill done

--- js/problems_pdf/promise_all.js
+++ js/problems_pdf/promise_all.js
@@ -7,24 +7,52 @@
 // ● The results are returned in the same order as the promises are in
 // the given array.
 
-const myPromiseAll = (allPromises) => {
+// const myPromiseAll = (allPromises) => {
+//   return new Promise((resolve, reject) => {
+//     const accumulatedResponse = [];
+//     for (let i = 0; i < allPromises.length; i++) {
+//       const currentPromise = allPromises[i];
+//       currentPromise
+//         .then((data) => {
+//           if (data) {
+//             accumulatedResponse.push(data);
+//           }
+//         })
+//         .catch((err) => {
+//           reject(err);
+//         });
+//     }
+//     resolve(accumulatedResponse);
+//   });
+// };
+
+const myPromiseAll = function (taskList) {
+  //to store results
+  const results = [];
+  //to track how many promises have completed
+  let promisesCompleted = 0;
+  // return new promise
   return new Promise((resolve, reject) => {
-    const accumulatedResponse = [];
-    for (let i = 0; i < allPromises.length; i++) {
-      const currentPromise = allPromises[i];
-      currentPromise
-        .then((data) => {
-          if (data) {
-            accumulatedResponse.push(data);
+    taskList.forEach((promise, index) => {
+      //if promise passes
+      promise
+        .then((val) => {
+          //store its outcome and increment the count
+          results[index] = val;
+          promisesCompleted += 1;
+          //if all the promises are completed,
+          //resolve and return the result
+          if (promisesCompleted === taskList.length) {
+            resolve(results);
           }
         })
-        .catch((err) => {
-          reject(err);
+        .catch((error) => {
+          reject(error);
         });
-    }
-    resolve(accumulatedResponse);
+    });
   });
 };
+//if any promise fails, reject.
 
 // Input:
 function task(time) {
