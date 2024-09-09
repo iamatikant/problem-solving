36a05de889e5c3deb03fcd55097de4bc2431a292
Atikant <atikant.ati@gmail.com>
2024-09-09 23:45:05 +0530

    this is another example

--- problems-solving_DSAlgo/merge.js
+++ problems-solving_DSAlgo/merge.js
@@ -76,3 +76,9 @@ console.log(merge(nums1, m, nums2, n));
 // const n = 3;
 // // Output: [1,2,2,3,5,6]
 // console.log(merge(nums1, m, nums2, n));
+// const nums1 = [1, 2, 3, 0, 0, 0];
+// const m = 3;
+// const nums2 = [2, 5, 6];
+// const n = 3;
+// // Output: [1,2,2,3,5,6]
+// console.log(merge(nums1, m, nums2, n));
