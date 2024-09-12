990305e4c96c11171b427e090d32b51ca40d08b2
Atikant <atikant.ati@gmail.com>
2024-09-13 00:02:53 +0530

    Buy sell problem

---
+++ problems-solving_DSAlgo/buySell.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let result = 0;
+  let min = prices[0];
+  for (let index = 0; index < prices.length; index++) {
+    const element = prices[index];
+    min = Math.min(prices[index], min);
+    result = Math.max(prices[index] - min, result);
+  }
+  return result;
+};
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
