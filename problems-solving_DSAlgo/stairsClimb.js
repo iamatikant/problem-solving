b14727c4434439c0f247a8431770a08f68dec2ef
Atikant <atikant.ati@gmail.com>
2024-09-12 23:55:02 +0530

    Climb stairs problem.

---
+++ problems-solving_DSAlgo/stairsClimb.js
@@ -0,0 +1,48 @@
+// You are climbing a staircase. It takes n steps to reach the top.
+
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+// Example 1:
+
+// Input: n = 2
+// Output: 2
+// Explanation: There are two ways to climb to the top.
+// 1. 1 step + 1 step
+// 2. 2 steps
+// Example 2:
+
+// Input: n = 3
+// Output: 3
+// Explanation: There are three ways to climb to the top.
+// 1. 1 step + 1 step + 1 step
+// 2. 1 step + 2 steps
+// 3. 2 steps + 1 step
+
+// Constraints:
+
+// 1 <= n <= 45
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+  if (n <= 2) {
+    return n;
+  }
+  let first = 1;
+  let second = 2;
+  let result = 0;
+  for (let i = 3; i <= n; i++) {
+    result = first + second;
+    first = second;
+    second = result;
+  }
+  return result;
+};
+
+console.log(climbStairs(4));
+console.log(climbStairs(5));
+console.log(climbStairs(6));
+console.log(climbStairs(7));
+console.log(climbStairs(8));
