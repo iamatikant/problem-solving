074abd868c74d97f20f65eab65186dec230d9c65
Atikant <atikant.ati@gmail.com>
2025-10-24 00:22:23 +0530

    Merge sort

---
+++ problems-solving_DSAlgo/mergeSort.js
@@ -0,0 +1,28 @@
+function mergeSort(nums) {
+  if (nums.length <= 1) return nums;
+
+  const mid = Math.floor(nums.length / 2);
+  const left = mergeSort(nums.slice(0, mid));
+  const right = mergeSort(nums.slice(mid));
+  return merge(left, right);
+}
+
+function merge(left, right) {
+  let result = [];
+  let i = 0;
+  let j = 0;
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      result.push(left[i]);
+      i++;
+    } else {
+      result.push(right[j]);
+      j++;
+    }
+  }
+
+  return result.concat(left.slice(i)).concat(right.slice(j));
+}
+
+const nums = [100, 4, 200, 1, 3, 2];
+console.log(mergeSort(nums));
